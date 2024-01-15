71f514c8cd87af4c8963f2dd95bbf0ccef08bee4
huanghai1 <huanghai1@xunlei.com>
2024-01-15 19:34:46 +0800

    1

--- services/run.go
+++ services/run.go
@@ -1,16 +1,37 @@
 package services
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/xlstudio/wxbizdatacrypt"
+	"io/ioutil"
 	"net/http"
 )
+
+func requestLoggerMiddleware(c *gin.Context) {
+	// 打印请求URI
+	fmt.Printf("Request URI: %s\n", c.Request.RequestURI)
+
+	// 读取请求体
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err == nil {
+		// 打印请求体
+		fmt.Printf("Request Body: %s\n", body)
+
+		// 重置请求体，确保后续处理中间件和路由处理函数可以正常读取
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	}
+
+	// 继续处理请求
+	c.Next()
+}
 
 func Run() {
 	server := gin.Default()
 
 	// Get 请求 返回 JSON
-	applet := server.Group("/wechat-server", CheckSource)
+	applet := server.Group("/wechat-server", requestLoggerMiddleware)
 	{
 		applet.POST("/revoke", func(c *gin.Context) {
 			params := VerifyWechatRequest{}
@@ -39,7 +60,7 @@ func Run() {
 	}
 
 	// 指定运行在 8080 端口
-	err := server.Run(":8081")
+	err := server.Run(":8080")
 	if err != nil {
 		return
 	}
